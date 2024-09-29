34c362e71d948474c3d99b80d27c55f80a48281a
LangatD <dorcaschepngetich20@gmail.com>
2024-09-29 08:10:50 +0300

    student grade generator added

---
+++ studentGradeGenerator/studentGradeGenerator.js
@@ -0,0 +1,17 @@
+//function studentGrades(marks)
+let marks = prompt("Enter student marks:");
+marks = Number(marks);
+//A > 79, B - 60 to 79, C -  49 to 59, D - 40 to 49, E - less 40.
+if (marks > 100 || marks< 0){
+    console.log("Inavlid input. Enter a number between 0 and 100")
+} else if (marks > 79){
+    console.log("Grade: A");
+} else if (marks >= 60){
+    console.log("Grade: B");
+} else if (marks >=49){
+    console.log("Grade: C");
+} else if (marks >= 40){
+    console.log("Grade: D");
+} else {
+    console.log("Grade: E");
+}
